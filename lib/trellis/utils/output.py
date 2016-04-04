aa13bc416e93fb1b246f1851b2bc70bae202296f
Phil Nelson <phil@fullyint.com>
2016-04-03 17:40:37 -0700

    Display system info with fail output

--- lib/trellis/utils/output.py
+++ lib/trellis/utils/output.py
@@ -2,11 +2,41 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import os.path
+import platform
+import re
 import textwrap
 
+from ansible import __version__
 from ansible.utils.unicode import to_unicode
 
-def display_output(result, action, display, first, task_failed=False):
+def system(vagrant_version=None):
+    # Get most recent Trellis CHANGELOG entry
+    changelog_msg = ''
+    changelog = os.path.join(os.getcwd(), 'CHANGELOG.md')
+
+    if os.path.isfile(changelog):
+        with open(changelog) as f:
+            str = f.read(200)
+
+        # Retrieve release number if it is most recent entry
+        release = re.search(r'^###\s((?!HEAD).*)', str)
+        if release is not None:
+            changelog_msg = '\n  Trellis {0}'.format(release.group(1))
+
+        # Retrieve most recent changelog entry
+        else:
+            change = re.search(r'.*\n\*\s*([^\(\n\[]+)', str)
+            if change is not None:
+                changelog_msg = '\n  Trellis at "{0}"'.format(change.group(1).strip())
+
+    # Vagrant info, if available
+    vagrant = ' Vagrant {0};'.format(vagrant_version) if vagrant_version else ''
+
+    # Assemble components and return
+    return 'System info:\n  Ansible {0};{1} {2}{3}'.format(__version__, vagrant, platform.system(), changelog_msg)
+
+def display_output(result, action, display, first, task_failed=False, vagrant_version=None):
     msg = ''
     result = result._result
     wrap_width = 77
@@ -39,8 +69,13 @@ def display_output(result, action, display, first, task_failed=False):
     msg = '\n'.join([textwrap.fill(line, wrap_width, replace_whitespace=False)
                      for line in msg.splitlines()])
 
-    # Display msg with horizontal rule between hosts/items
+    # Display system info and msg, with horizontal rule between hosts/items
     hr = '-' * int(wrap_width*.67)
+
+    if task_failed and first:
+        display(system(vagrant_version), 'bright gray')
+        display(hr, 'bright gray')
+
     if msg == '':
         if task_failed and not first:
             display(hr, 'bright gray')
