fd774ee4cf7e8baa2d146244711bb89ebb3811b4
Phil Nelson <phil@fullyint.com>
2016-04-09 14:39:53 -0600

    Add custom output options to ansible.cfg

--- lib/trellis/plugins/callback/output.py
+++ lib/trellis/plugins/callback/output.py
@@ -7,6 +7,7 @@ import sys
 
 from ansible.parsing.dataloader import DataLoader
 from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
+from ansible.utils.unicode import to_unicode
 
 try:
     from trellis.utils import output as output
@@ -25,6 +26,7 @@ class CallbackModule(CallbackModule_default):
 
     def __init__(self):
         super(CallbackModule, self).__init__()
+        output.load_configs(self)
         output.reset_task_info(self)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
@@ -47,7 +49,8 @@ class CallbackModule(CallbackModule_default):
 
     def v2_playbook_on_task_start(self, task, is_conditional):
         output.reset_task_info(self, task)
-        super(CallbackModule, self).v2_playbook_on_task_start(task, is_conditional)
+        if self.display_include_tasks or task._get_parent_attribute('action') != 'include':
+            super(CallbackModule, self).v2_playbook_on_task_start(task, is_conditional)
 
     def v2_playbook_on_handler_task_start(self, task):
         output.reset_task_info(self, task)
@@ -62,18 +65,28 @@ class CallbackModule(CallbackModule_default):
         if 'vagrant_version' in play_vars:
             self.vagrant_version = play_vars['vagrant_version']
 
+    def v2_playbook_on_stats(self, stats):
+        super(CallbackModule, self).v2_playbook_on_stats(stats)
+        self._display.display('To manage Trellis CLI output, see https://roots.io/trellis/docs/cli-output/\n', 'bright gray')
+
+    def v2_playbook_on_include(self, included_file):
+        if self.display_include_tasks:
+            super(CallbackModule, self).v2_playbook_on_include(included_file)
+
     def v2_playbook_item_on_ok(self, result):
         output.display_item(self, result)
         output.replace_item_with_key(self, result)
+        output.truncate_item(self, result)
         super(CallbackModule, self).v2_playbook_item_on_ok(result)
 
     def v2_playbook_item_on_failed(self, result):
         self.task_failed = True
         output.display_item(self, result)
-        output.replace_item_with_key(self, result)
         super(CallbackModule, self).v2_playbook_item_on_failed(result)
 
     def v2_playbook_item_on_skipped(self, result):
-        output.display_item(self, result)
-        output.replace_item_with_key(self, result)
-        super(CallbackModule, self).v2_playbook_item_on_skipped(result)
+        if self.display_skipped_items:
+            output.display_item(self, result)
+            output.replace_item_with_key(self, result)
+            output.truncate_item(self, result)
+            super(CallbackModule, self).v2_playbook_item_on_skipped(result)
