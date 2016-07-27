a1da2804a48190618d60b509f9c8fa493c649073
Shawn Mosher <shawn@theaggressive.com>
2016-07-26 17:42:27 -0700

    Updated windows.sh (#623)
    Fix #621 - Add apt update before installing pip in windows.sh

--- windows.sh
+++ windows.sh
@@ -32,6 +32,7 @@ fi
 # Install Ansible and its dependencies if not installed.
 if [ ! -f /usr/bin/ansible ]; then
   echo "Installing pip..."
+  sudo apt-get -y update
   sudo apt-get -y install python-pip
   echo "Installing Ansible with pip..."
   sudo pip install ansible=='2.0.2.0'
