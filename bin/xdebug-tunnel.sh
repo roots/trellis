ec6d9e34e045b0f67959da62c3e93d2fdd124d34
Nathaniel Schweinberg <natanielks@gmail.com>
2016-10-27 09:46:28 -0700

    remove xdebug-tunnel dependency on env, update ansible_ssh_* to ansible_*, update tunnel script to use hosts: localhost

--- bin/xdebug-tunnel.sh
+++ bin/xdebug-tunnel.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # Usage:
-#   To open a tunnel: bin/xdebug-tunnel.sh production example_com_prod
-#   To close a tunnel: bin/xdebug-tunnel.sh production example_com_prod close
+#   To open a tunnel: bin/xdebug-tunnel.sh example_com_prod
+#   To close a tunnel: bin/xdebug-tunnel.sh example_com_prod close
 
-ENV="-e env=${1}"
-SSH_HOST="-e xdebug_tunnel_ssh_host=${2}"
-MAYBE_CLOSE="${3}"
+SSH_HOST="-e xdebug_tunnel_ssh_host=${1}"
+MAYBE_CLOSE="${2}"
 CLOSE_CONNECTION=
 
 if [[ "${MAYBE_CLOSE}" == 'close' ]]; then
   CLOSE_CONNECTION="-e xdebug_tunnel_close=true"
 fi
 
-ansible-playbook xdebug-tunnel.yml ${ENV} ${SSH_HOST} ${CLOSE_CONNECTION}
+ansible-playbook xdebug-tunnel.yml ${SSH_HOST} ${CLOSE_CONNECTION}
