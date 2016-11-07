571c3f3906047f80278e34b3f63c6f8eb9b3ea46
Nathaniel Schweinberg <natanielks@gmail.com>
2016-11-07 09:40:51 -0800

    Move Xdebug setup to new role and create xdebug-tunnel role

---
+++ xdebug-tunnel.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Usage:
+#   To open a tunnel: bin/xdebug-tunnel.sh example_com_prod
+#   To close a tunnel: bin/xdebug-tunnel.sh example_com_prod close
+
+SSH_HOST="-e xdebug_tunnel_inventory_host=${1}"
+MAYBE_CLOSE="${2}"
+CLOSE_CONNECTION=
+DEBUG="${DEBUG:-}"
+VERBOSITY="${VERBOSITY:--vvvv}"
+PARAMs="${PARAMS:-}"
+
+if [[ "${MAYBE_CLOSE}" == 'close' ]]; then
+  PARAMS="${PARAMS} -e xdebug_tunnel_close=true -e xdebug_install=false"
+else
+  PARAMS="${PARAMS} -e xdebug_install=true -e xdebug_remote_enable=1"
+fi
+
+if [[ -n "${DEBUG}" ]]; then
+  PARAMS="${PARAMS} ${VERBOSITY}"
+fi
+
+PARAMS="${SSH_HOST} ${CLOSE_CONNECTION} ${PARAMS}"
+ansible-playbook xdebug-tunnel.yml ${PARAMS}
