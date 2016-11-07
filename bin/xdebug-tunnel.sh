ba0f5fdecb072b3de9580933b9ed6d674607e97e
Nathaniel Schweinberg <natanielks@gmail.com>
2016-11-07 11:03:25 -0800

    Move xdebug-tunnel.sh to bin/

--- bin/xdebug-tunnel.sh
+++ bin/xdebug-tunnel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Usage:
-#   To open a tunnel: ./xdebug-tunnel.sh example_com_prod
-#   To close a tunnel: ./xdebug-tunnel.sh example_com_prod close
+#   To open a tunnel: bin/xdebug-tunnel.sh example_com_prod
+#   To close a tunnel: bin/xdebug-tunnel.sh example_com_prod close
 
 SSH_HOST="-e xdebug_tunnel_inventory_host=$1"
 XDEBUG_ENABLE="-e xdebug_remote_enable=$([[ $2 == "close" ]] && echo 0 || echo 1)"
