4bf820acfed1740303b70017ad408b47085ebc38
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2017-04-23 16:50:05 -0400

    Add options for sources and version

--- lib/trellis/vagrant.rb
+++ lib/trellis/vagrant.rb
@@ -11,11 +11,19 @@ def ensure_plugins(plugins)
   installed = false
 
   plugins.each do |plugin|
+    plugin_name = plugin['name']
     manager = Vagrant::Plugin::Manager.instance
-    next if manager.installed_plugins.has_key?(plugin)
 
-    logger.warn("Installing plugin #{plugin}")
-    manager.install_plugin(plugin, sources: Vagrant::Bundler::DEFAULT_GEM_SOURCES)
+    next if manager.installed_plugins.has_key?(plugin_name)
+
+    logger.warn("Installing plugin #{plugin_name}")
+
+    manager.install_plugin(
+      plugin_name,
+      sources: plugin.fetch('source', %w(https://rubygems.org/ https://gems.hashicorp.com/)),
+      version: plugin['version']
+    )
+
     installed = true
   end
 
