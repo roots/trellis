109499e26ca401ebddda0c7b793af824917efb43
Tang Rufus <tangrufus@gmail.com>
2017-09-19 02:59:48 +0800

    Extract Trellis::Config (#890)

--- lib/trellis/vagrant.rb
+++ lib/trellis/vagrant.rb
@@ -37,43 +37,12 @@ def fail_with_message(msg)
   fail Vagrant::Errors::VagrantError.new, msg
 end
 
-def hosts(sites)
-  site_hosts = sites.flat_map { |(_name, site)| site['site_hosts'] }
-
-  site_hosts.each do |host|
-    if !host.is_a?(Hash) || !host.has_key?('canonical')
-      fail_with_message File.read(File.join(ANSIBLE_PATH, 'roles/common/templates/site_hosts.j2')).sub!('{{ env }}', 'development').gsub!(/com$/, 'dev')
-    end
-  end
-
-  site_hosts
-end
-
-def load_wordpress_sites
-  config_file = File.join(ANSIBLE_PATH, 'group_vars', 'development', 'wordpress_sites.yml')
-
-  if File.exists?(config_file)
-    wordpress_sites = YAML.load_file(config_file)['wordpress_sites']
-    fail_with_message "No sites found in #{config_file}." if wordpress_sites.to_h.empty?
-  else
-    fail_with_message "#{config_file} was not found. Please set `ANSIBLE_PATH` in your Vagrantfile."
-  end
-
-  wordpress_sites
-end
-
 def local_provisioning?
   @local_provisioning ||= Vagrant::Util::Platform.windows? || !which('ansible-playbook') || ENV['FORCE_ANSIBLE_LOCAL']
 end
 
 def local_site_path(site)
   File.expand_path(site['local_path'], ANSIBLE_PATH)
-end
-
-def multisite_subdomains?(wordpress_sites)
-  wordpress_sites.any? do |(_name, site)|
-    site['multisite'].fetch('enabled', false) && site['multisite'].fetch('subdomains', false)
-  end
 end
 
 def nfs_path(path)
