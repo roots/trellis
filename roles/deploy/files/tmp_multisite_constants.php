3bea855b015acd209e4c0bbf9931a8e6897e02ce
strarsis <strarsis@gmail.com>
2022-06-12 13:05:52 +0200

    Remove unused `tmp_multisite_constants.php` source file.

--- roles/deploy/files/tmp_multisite_constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL & ~E_NOTICE);
-define('MULTISITE', false);
-define('SUBDOMAIN_INSTALL', false);
-define('WPMU_PLUGIN_DIR', null);
-define('WP_PLUGIN_DIR', null);
-define('WP_USE_THEMES', false);
